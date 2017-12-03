833b487a6d8ad67f4c2a1ef0f190dec2842d8838
Muhammad Irfan <41815110165@student.mercubuana.ac.id>
2017-12-03 21:02:07 +0700

    Update
    Tambah fungsi upload dari excel

--- application/controllers/Network_Backbone.php
+++ application/controllers/Network_Backbone.php
@@ -10,6 +10,9 @@ class Network_Backbone extends MY_Controller {
 		parent::__construct();
 		$this->load->model('model_backbone');
 		$this->load->model('model_backbone_detail');
+
+		// load custom library
+		$this->load->library(array('PHPExcel','PHPExcel/IOFactory'));
 	}
 	
 	public function index() {
@@ -194,6 +197,57 @@ class Network_Backbone extends MY_Controller {
 
 		$this->index();
 	}
+
+	public function upload(){
+        $fileName = time().$_FILES['file']['name'];
+         
+        $config['upload_path'] = './assets/'; //buat folder dengan nama assets di root folder
+        $config['file_name'] = $fileName;
+        $config['allowed_types'] = 'xls|xlsx|csv';
+        $config['max_size'] = 10000;
+         
+        $this->load->library('upload');
+        $this->upload->initialize($config);
+         
+        if(! $this->upload->do_upload('file') )
+        $this->upload->display_errors();
+             
+        $media = $this->upload->data('file');
+        $inputFileName = './assets/'.$media['file_name'];
+         
+        try {
+                $inputFileType = IOFactory::identify($inputFileName);
+                $objReader = IOFactory::createReader($inputFileType);
+                $objPHPExcel = $objReader->load($inputFileName);
+            } catch(Exception $e) {
+                die('Error loading file "'.pathinfo($inputFileName,PATHINFO_BASENAME).'": '.$e->getMessage());
+            }
+ 
+            $sheet = $objPHPExcel->getSheet(0);
+            $highestRow = $sheet->getHighestRow();
+            $highestColumn = $sheet->getHighestColumn();
+             
+            for ($row = 2; $row <= $highestRow; $row++){                  //  Read a row of data into an array                 
+                $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row,
+                                                NULL,
+                                                TRUE,
+                                                FALSE);
+                                                 
+                //Sesuaikan sama nama kolom tabel di database                                
+                 $data = array(
+                    "idimport"=> $rowData[0][0],
+                    "nama"=> $rowData[0][1],
+                    "alamat"=> $rowData[0][2],
+                    "kontak"=> $rowData[0][3]
+                );
+                 
+                //sesuaikan nama dengan nama tabel
+                $insert = $this->db->insert("eimport",$data);
+                delete_files($media['file_path']);
+                     
+            }
+        redirect('manage/backbone');
+    }
 }
 
 /* End of file Network_Backbone.php */
